d19b10a4a944e9fb460c40b8ef3871dbeab70518
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2021-12-12 14:20:31 +0700

    ZStack and backgroundUpdated

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -12,27 +12,34 @@ struct ContentView: View {
     @State private var backgroundUpdated = false
     var body: some View {
         NavigationView {
-            VStack {
-                Button(action: {
-                    self.alertIsPresented = true
-                }, label: {
-                    Text("Tap Me!")
-                        .foregroundColor(.white)
-                        .bold()
-                        .font(.system(size: 24))
-                })
-                    .frame(width: 200,
-                           height: 50,
-                           alignment: /*@START_MENU_TOKEN@*/ .center/*@END_MENU_TOKEN@*/)
-                    .padding()
-                    .background(Color.green)
-                    .cornerRadius(8)
-                    .alert(isPresented: $alertIsPresented,
-                           content: {
-                               Alert(title: Text("Would you like to purchase ?"), primaryButton: .default(Text("Purchase"), action: {
-                                   print("start purchase")
-                               }), secondaryButton: .cancel(Text("No, Thanks")))
-                           })
+            ZStack {
+                if backgroundUpdated {
+                    Color.red
+                } else {
+                    Color.blue
+                }
+                VStack {
+                    Button(action: {
+                        self.alertIsPresented = true
+                    }, label: {
+                        Text("Tap Me!")
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.system(size: 24))
+                    })
+                        .frame(width: 200,
+                               height: 50,
+                               alignment: /*@START_MENU_TOKEN@*/ .center/*@END_MENU_TOKEN@*/)
+                        .padding()
+                        .background(Color.green)
+                        .cornerRadius(8)
+                        .alert(isPresented: $alertIsPresented,
+                               content: {
+                                   Alert(title: Text("Would you like to purchase ?"), primaryButton: .default(Text("Purchase"), action: {
+                                    self.backgroundUpdated.toggle()
+                                   }), secondaryButton: .cancel(Text("No, Thanks")))
+                               })
+                }
             }
             .navigationTitle("SwiftUI Alerts")
         }
