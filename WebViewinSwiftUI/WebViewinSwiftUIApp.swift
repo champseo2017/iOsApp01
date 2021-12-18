485cddf2100b920cf0bb0b70e93fc3144ab159fb
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2021-12-18 14:06:45 +0700

    WebView Done

---
+++ WebViewinSwiftUI/WebViewinSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewinSwiftUIApp.swift
+//  WebViewinSwiftUI
+//
+//  Created by Champ on 18/12/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct WebViewinSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
