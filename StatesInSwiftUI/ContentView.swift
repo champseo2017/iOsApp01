346216c0420cc99b561d70083510a508768354d8
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2021-12-22 10:18:37 +0700

    SwiftUI: Bindings (State) Tutorial 13:29
    https://www.youtube.com/watch?v=5G7IVEYtaxo&list=PL5PR3UyfTWvfgx9W8WJ9orQf6N1tx0oxN&index=14

--- StatesInSwiftUI/ContentView.swift
+++ StatesInSwiftUI/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var isZoomed: Bool = false
-    var body: some View {
+//    @State var isZoomed: Bool = false
+    let images: [Image] = [
+        Image("Image"),
+        Image("Image"),
+        Image("Image"),
+        Image("Image"),
         Image("Image")
-            .resizable()
-            .aspectRatio(contentMode: isZoomed ? .fill : .fit)
-            .onTapGesture {
-                self.isZoomed.toggle()
+    ]
+    var body: some View {
+        NavigationView {
+            List(0..<5) { index in
+                NavigationLink(destination: MyImageView(image: self.images[index])) {
+                    Text("Item \(index + 1)")
+                }
             }
+        }
+//        Image("Image")
+//            .resizable()
+//            .aspectRatio(contentMode: isZoomed ? .fill : .fit)
+//            .onTapGesture {
+//                withAnimation {
+//                  self.isZoomed.toggle()
+//                }
+//            }
     }
 }
 
@@ -23,5 +39,12 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+    }
+}
+
+struct MyImageView: View {
+    let image: Image
+    var body: some View {
+        image
     }
 }
