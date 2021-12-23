c2d62bc8c86810d3f471db84ec9b15aa24c59389
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2021-12-23 09:26:17 +0700

    SwiftUI: Bindings (State) Done
    https://www.youtube.com/watch?v=5G7IVEYtaxo&list=PL5PR3UyfTWvfgx9W8WJ9orQf6N1tx0oxN&index=15

--- StatesInSwiftUI/ContentView.swift
+++ StatesInSwiftUI/ContentView.swift
@@ -11,15 +11,15 @@ struct ContentView: View {
 //    @State var isZoomed: Bool = false
     let images: [Image] = [
         Image("Image"),
+        Image(systemName: "bookmark"),
+        Image(systemName: "ear"),
         Image("Image"),
         Image("Image"),
-        Image("Image"),
-        Image("Image")
     ]
     var body: some View {
         NavigationView {
-            List(0..<5) { index in
-                NavigationLink(destination: MyImageView(image: self.images[index])) {
+            List(0 ..< 5) { index in
+                NavigationLink(destination: MyImageView(image: self.images[index], index: index)) {
                     Text("Item \(index + 1)")
                 }
             }
@@ -44,7 +44,29 @@ struct ContentView_Previews: PreviewProvider {
 
 struct MyImageView: View {
     let image: Image
+    @State var index: Int
+    let colors = [
+        Color.red,
+        Color.blue,
+        Color.green,
+        Color.purple,
+        Color.orange,
+    ]
     var body: some View {
-        image
+        VStack {
+            Spacer()
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Spacer()
+        }
+        .background(colors[index])
+        .onTapGesture {
+            if self.index < 4 {
+                self.index += 1
+            } else {
+                self.index = 0
+            }
+        }
     }
 }
