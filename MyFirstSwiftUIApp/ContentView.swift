d1381344f3b9a652cee0eecdd092ab988b4aff61
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2021-12-25 12:19:00 +0700

    SwiftUI for Beginners 14:37
    https://www.youtube.com/watch?v=G4RN3tCdpO0&list=PL5PR3UyfTWvfgx9W8WJ9orQf6N1tx0oxN&index=15

--- MyFirstSwiftUIApp/ContentView.swift
+++ MyFirstSwiftUIApp/ContentView.swift
@@ -6,19 +6,41 @@
 //
 
 import SwiftUI
-
+struct Data: Identifiable {
+    var id = UUID()
+    let title: String
+    let imageName: String
+    let imageDescription: String
+    let imageTakenDate: String
+}
 struct ContentView: View {
+    var items = [Data]()
     var body: some View {
-        List(0 ..< 10) { _ in
-            Text("Hello, World")
-                .foregroundColor(.red)
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(items) { data in
+                NavigationLink(destination: Text("Some text")) {
+                    Image(data.imageName)
+                        .resizable().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Text(data.title)
+                        .foregroundColor(.primary)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                }
+            }
+            .navigationTitle("Photos")
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(items: [
+            Data(title: "Beach 1", imageName: "Beach1", imageDescription: "This is a beach", imageTakenDate: "1/1/2020"),
+            Data(title: "Beach 1", imageName: "Beach2", imageDescription: "This is a beach", imageTakenDate: "1/1/2020"),
+            Data(title: "Beach 1", imageName: "Beach3", imageDescription: "This is a beach", imageTakenDate: "1/1/2020"),
+            Data(title: "Beach 1", imageName: "Beach4", imageDescription: "This is a beach", imageTakenDate: "1/1/2020"),
+            Data(title: "Beach 1", imageName: "Beach5", imageDescription: "This is a beach", imageTakenDate: "1/1/2020"),
+            Data(title: "Beach 1", imageName: "Beach6", imageDescription: "This is a beach", imageTakenDate: "1/1/2020"),
+            
+        ])
     }
 }
