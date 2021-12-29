af38c7e3dc5a9df3588018b8dba95e9662f9b6d7
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2021-12-29 10:34:20 +0700

    SwiftUI Forms & TextFields Tutorial Done
    https://www.youtube.com/watch?v=iWtK4Foy1ZI&list=PL5PR3UyfTWvfgx9W8WJ9orQf6N1tx0oxN&index=16

--- SwiftUIForms/ContentView.swift
+++ SwiftUIForms/ContentView.swift
@@ -7,32 +7,44 @@
 
 import SwiftUI
 
-struct ContentView: View {
+class FormViewModel: ObservableObject {
     @State var firstName = ""
     @State var lastName = ""
     @State var password = ""
     @State var passwordAgain = ""
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = FormViewModel()
     var body: some View {
         NavigationView {
             VStack {
                 Form {
                     Section {
-                        TextField("First Name", text: $firstName)
-                        TextField("Last Name", text: $lastName)
+                        TextField("First Name", text: $viewModel.firstName)
+                        TextField("Last Name", text: $viewModel.lastName)
                     }
                     Section(footer: Text("Your password must be at least 8 characters long.")) {
-                        SecureField("Create Password", text: $password)
-                        SecureField("Confirm Password", text: $passwordAgain)
+                        SecureField("Create Password", text: $viewModel.password)
+                        SecureField("Confirm Password", text: $viewModel.passwordAgain)
+                    }
+                    Section(header: Text("Mailing Address")) {
+                        TextField("Street Address", text: $viewModel.firstName)
+                        TextField("City", text: $viewModel.firstName)
+                        TextField("State", text: $viewModel.firstName)
+                        TextField("Postal Code", text: $viewModel.firstName)
+                        TextField("Country", text: $viewModel.firstName)
                     }
                 }
+                Divider()
                 Button(action: {}, label: {
                     Text("Continue")
-                        .frame(width: 200, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .frame(width: 200, height: 50, alignment: /*@START_MENU_TOKEN@*/ .center/*@END_MENU_TOKEN@*/)
                         .background(Color.blue)
                         .foregroundColor(.white)
                         .cornerRadius(8)
                 })
-                Spacer()
+                    .padding()
             }
             .navigationTitle("Create Account")
         }
