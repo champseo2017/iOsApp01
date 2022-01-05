86e298d92d529d74f3669a16cbc5d916abd37b2e
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2022-01-05 21:43:57 +0700

    SwiftUI Activity Done

---
+++ SwiftUIActivityIndicators/SwiftUIActivityIndicatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIActivityIndicatorsApp.swift
+//  SwiftUIActivityIndicators
+//
+//  Created by Champ on 5/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIActivityIndicatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
