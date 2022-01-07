a3f406c38e28bd141270bd26f824b6e62189884f
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2022-01-07 21:00:55 +0700

    Swift Ui Custom Icons Done

--- SwiftUICustomIcons/ContentView.swift
+++ SwiftUICustomIcons/ContentView.swift
@@ -5,19 +5,38 @@
 //  Created by Champ on 5/1/2565 BE.
 //
 
-import SwiftUIFontIcon
 import SwiftUI
+import SwiftUIFontIcon
 
 struct ContentView: View {
+    @State var updateBg = false
     var body: some View {
         NavigationView {
-            VStack {
-                HStack {
-                    Text("Material Icon")
-                    Spacer()
-                    FontIcon.text(.materialIcon(code: .assistant), fontsize: 45, color: .blue)
-                }.padding()
+            ZStack {
+                VStack {
+                    HStack {
+                        Text("Material Icon")
+                        Spacer()
+                        FontIcon.text(.materialIcon(code: .assistant), fontsize: 45, color: .blue)
+                    }.padding()
+                    
+                    HStack {
+                        Text("Material Icon")
+                        Spacer()
+                        FontIcon.text(.materialIcon(code: MaterialIconCode.sync), fontsize: 45, color: .green)
+                    }.padding()
+                    
+                    HStack {
+                        Text("Material Icon")
+                        Spacer()
+                        FontIcon.button(.ionicon(code: .ios_add_circle), action: {
+                            self.updateBg.toggle()
+                        }, padding: 0, fontsize: 45, color: .pink)
+                    }.padding()
+                    
+                }
             }
+            .background(updateBg ? Color.pink: Color(.systemBackground))
             .navigationTitle("Custom Icons")
         }
     }
